77a8658688764d046cd7c67116e28b7033f760cb
Mert KARAYEL <mertkarayel94@hotmail.com>
2016-09-21 17:50:56 +0000

    Timestamp microservice

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require("express")
 var app = express()
+var date = require('date-and-time')
 
-app.get('/',function(request,response){
-    response.end('Hello World');
-})
+var options = {
+  index: "index.html"
+};
+
+app.get('/:id', function(request, response){
+    var data = request.params.id
+    var res;
+    if(parseInt(data) || data === "0"){
+        res = {unix:parseInt(data),natural:date.format(new Date(parseInt(data) * 1000), 'MMMM DD, YYYY')};
+    }else{
+        if(date.isValid(data,'MMMM DD, YYYY')){
+            var time = new Date(data).getTime()/100
+            res = {unix:time,natural:data} 
+        }else{
+            res = {unix:null,natural:null}
+        }
+    }
+   response.json(res);
+});
+
+app.use('/', express.static('public', options));
 
 app.listen(8080);
+
